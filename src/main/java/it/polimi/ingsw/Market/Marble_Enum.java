7f146df7fae897b25e4c8f16e9d6bc75252eab1b
giamma <gianmarco.frangipane@mail.polimi.it>
2021-04-01 22:25:41 +0200

    Added Market elements and relative tests

---
+++ src/main/java/it/polimi/ingsw/Market/Marble_Enum.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.Market;
+
+/**
+ * Simple enum for the types of marbles in the market
+ */
+public enum Marble_Enum {
+    WHITE, RED, BLUE, YELLOW, GREY, PURPLE
+}
---
+++ src/main/java/it/polimi/ingsw/Market/MarketMarble.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Market;
+ import java.util.Random;
+
+/**
+ * Class that implements the marbles in the market
+ */
+
+public class MarketMarble {
+    private Marble_Enum marbleColor;
+
+
+
+    public Marble_Enum getMarbleColor() {
+        return marbleColor;
+    }
+
+    public void setMarbleColor(Marble_Enum marbleColor) {
+        this.marbleColor = marbleColor;
+    }
+    /**
+     * Method use to convert a marble into the corresponding resource
+     */
+    //public Production convertRes(int idPlay){}
+
+
+}
