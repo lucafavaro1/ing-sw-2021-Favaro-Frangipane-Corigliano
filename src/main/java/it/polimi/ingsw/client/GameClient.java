6eb9c1d5da7e0d4f5f07835789f2b2947dfae398
lucafavaro1 <luca1.favaro@mail.polimi.it>
2021-04-21 11:21:50 +0200

    First attempt of a client/server working example

--- src/main/java/it/polimi/ingsw/client/GameClient.java
+++ src/main/java/it/polimi/ingsw/client/GameClient.java
@@ -1,6 +1,78 @@
 package it.polimi.ingsw.client;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class GameClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // acquisizione dell'ip del client che esegue il programma
+        InetAddress addr;
+        if (args.length == 0) addr = InetAddress.getByName(null);
+
+        else addr = InetAddress.getByName(args[0]);
+
+        // definizione della client socket e buffer per in/out socket e stdin
+        Socket clientSocket = null;
+        BufferedReader in = null, stdIn=null;
+        PrintWriter out = null;
+        int port = 0;
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("<< Client Login >>");
+
+        System.out.println("Insert server port you want to connect (mandatory > 1024):");
+
+        // lettura della porta su cui connettersi
+        try {
+            port = Integer.parseInt(stdIn.readLine());
+        } catch (InputMismatchException e) {
+            System.err.println("Numeric format requested, application is closing");
+            System.exit(-1);
+        }
+
+        // tentativo di connessione al server su quella porta
+        System.out.println("Connecting to port " + port);
+        try {
+            clientSocket = new Socket(addr, port);
+            System.out.println("Connected!");
+        } catch (IOException e) {
+            System.err.println("Accept failed");
+            System.exit(1);
+        }
+
+        // creazione stream di input da socket
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        // creazione stream di output su socket
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        out = new PrintWriter(bw, true);
+
+        //stream di output da tastiera è StdIn definito sopra
+        String userInput = "";
+        String str = "";
+
+        System.out.println("Inizio comunicazione con il server: ");
+
+        // ciclo di send message e recive answer dal client al server
+        try {
+            while (true){
+                userInput = stdIn.readLine();
+                out.println(userInput);
+                if (userInput.equals("END")) break;
+                str = in.readLine();
+                System.out.println("Server message: " + str);
+                if (str.equals("END")) break;
+            }
+        } catch (IOException e) {
+            System.err.println("Couldn’t get I/O for the connection to: " + addr);
+            System.exit(1);
+        }
+
+        System.out.println("Client closing...");
+        out.close();
+        in.close();
+        stdIn.close();
+        clientSocket.close();
     }
 }
--- src/main/java/it/polimi/ingsw/server/GameServer.java
+++ src/main/java/it/polimi/ingsw/server/GameServer.java
@@ -1,8 +1,93 @@
 package it.polimi.ingsw.server;
 
-public class GameServer {
-    public static void main(String[] args) {
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
+/**
+ * TODO: pensare a come far si che il server sia sempre in ascolto (anche mentre sta mandando lui messaggio)
+ * TODO: aggiungere più client connessi allo stesso server
+ * TODO: implementare il multithreading per server e client
+ */
+
+public class GameServer {
+    public static void main(String[] args) throws Exception {
+        // definizione delle socket + buffer per lettura scrittura sia su socket che StdIn
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader in = null, stdIn=null;
+        PrintWriter out = null;
+        stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("<< Server Login >>");
+        System.out.println("Insert server port (mandatory > 1024): ");
+
+        // scelta della porta su cui far partire il server
+        int port = 0;
+        try {
+            port = Integer.parseInt(stdIn.readLine());
+        } catch (InputMismatchException e) {
+            System.err.println("Numeric format requested, application is closing");
+            System.exit(-1);
+
+        }
+        if (port < 0 || (port > 0 && port < 1024)) {
+            System.err.println("Error: ports accepted started from 1024! Please insert a new value.");
+            main(null);
+        }
+
+        // accensione del server
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Accepting...");
+
+        // connessione con il client
+        try {
+            clientSocket = serverSocket.accept();
+            System.out.println("The client "+ clientSocket.getInetAddress() + "was accepted");
+        } catch (IOException e) {
+            System.err.println("There's no server ON at this port! ");
+            System.exit(1);
+        }
+
+        // creazione stream di input da clientSocket
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        // creazione stream di output su clientSocket
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        out = new PrintWriter(bw, true);
+
+        //stream di output da tastiera è StdIn definito sopra
+        String serverInput = "";
+        String str = "";
+
+        System.out.println("Inizio comunicazione con il client: ");
+
+        //ciclo di ricezione dal client e invio di risposta
+        try{
+            while (true) {
+                str = in.readLine();
+                System.out.println("Client message: " + str);
+                if (str.equals("END")) break;
+                serverInput = stdIn.readLine();
+                out.println(serverInput);
+                if (serverInput.equals("END")) break;
+            }
+        } catch (IOException e) {
+            System.err.println("Couldn’t get I/O for the connection to: " + clientSocket.getInetAddress());
+            System.exit(1);
+        }
+
+
+        System.out.println("Server closing...");
+        stdIn.close();
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 
 }
