23cf9b3ea387e0a9579e0ba7cd2a379e32f106b8
lucafavaro1 <luca1.favaro@mail.polimi.it>
2021-05-04 15:34:49 +0200

    -Minor repo structure changes (adding gui) -Pom modified in order to add JavaFx support -First example of GUI with scene builder

---
+++ src/main/java/it/polimi/ingsw/client/gui/GUI.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.client.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+    private static final String prova = "/client/prova.fxml";
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../../../../../resources/Client/prova.fxml"));
+        Parent root = (Parent) loader.load();
+        stage.setTitle("Prova dell'applicazione");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.show();
+    }
+}
